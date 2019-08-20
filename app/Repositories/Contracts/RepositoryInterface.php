048406627e2f832e423e57294551ab8f26ff7674
MohamedPHP <mohamedzayed709@yahoo.com>
2019-08-20 18:20:00 +0200

    Start at 4

--- app/Repositories/Contracts/RepositoryInterface.php
+++ app/Repositories/Contracts/RepositoryInterface.php
@@ -2,6 +2,27 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 interface RepositoryInterface
 {
+    public function all() : Collection;
+
+    public function find(int $id) : Model;
+
+    public function findMany(array $ids) : Collection;
+
+    public function findWhere(string $column, string $value) : Collection;
+
+    public function findWhereFirst(string $column, string $value) : Model;
+
+    public function paginate(int $limit = 10) : LengthAwarePaginator;
+
+    public function create(array $data) : Model;
+
+    public function update(int $id, array $data) : bool;
+
+    public function delete(int $id) : bool;
 }
--- app/Repositories/RepositoryAbstract.php
+++ app/Repositories/RepositoryAbstract.php
@@ -3,12 +3,18 @@
 namespace App\Repositories;
 
 use App\Repositories\Contracts\RepositoryInterface;
+
 use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 
 abstract class RepositoryAbstract implements RepositoryInterface
 {
     /**
-     * The Entity
+     * The Entity.
+     *
+     * @var Model
      */
     protected $entity;
 
@@ -28,6 +34,97 @@ abstract class RepositoryAbstract implements RepositoryInterface
     public function all() : Collection
     {
         return $this->entity->all();
+    }
+
+    /**
+     * Find Entity By ID.
+     *
+     * @param  int   $id
+     * @return Model
+     */
+    public function find(int $id) : Model
+    {
+        return $this->entity->find($id);
+    }
+
+    /**
+     * Find Many Entities By ID's.
+     *
+     * @param  array      $ids
+     * @return Collection
+     */
+    public function findMany(array $ids) : Collection
+    {
+        return $this->entity->findMany($ids);
+    }
+
+    /**
+     * Find $column Where Equals $value.
+     *
+     * @param  string     $column
+     * @param  string     $value
+     * @return Collection
+     */
+    public function findWhere(string $column, string $value) : Collection
+    {
+        return $this->entity->where($column, $value)->get();
+    }
+
+    /**
+     * Find $column Where Equals $value then get the first value.
+     *
+     * @param  string $column
+     * @param  string $value
+     * @return Model
+     */
+    public function findWhereFirst(string $column, string $value) : Model
+    {
+        return $this->entity->where($column, $value)->first();
+    }
+
+    /**
+     * Paginate.
+     *
+     * @param  integer   $limit
+     * @return Paginator
+     */
+    public function paginate(int $limit = 10) : LengthAwarePaginator
+    {
+        return $this->entity->paginate($limit);
+    }
+
+    /**
+     * Create New Entity.
+     *
+     * @param  array $data
+     * @return Model
+     */
+    public function create(array $data) : Model
+    {
+        return $this->entity->create($data);
+    }
+
+    /**
+     * Update Entity.
+     *
+     * @param  integer $id
+     * @param  array   $data
+     * @return boolean
+     */
+    public function update(int $id, array $data) : bool
+    {
+        return $this->find($id)->update($data);
+    }
+
+    /**
+     * Delete Entity.
+     *
+     * @param integer $id
+     * @return boolean
+     */
+    public function delete(int $id) : bool
+    {
+        return $this->find($id)->delete();
     }
 
     /**
